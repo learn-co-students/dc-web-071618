0c7294dce417a07a182490899cca7ff3d8e12b72
Rob Cobb <rwcobbjr@gmail.com>
2018-07-17 12:25:22 -0400

    hashes and the internet

--- 02-hashes-and-the-internet/README.md
+++ 02-hashes-and-the-internet/README.md
@@ -5,12 +5,56 @@
 * GET requests
 * Server responses
 * Developer console
+* Gems
 * APIs
 * JSON (string formatted as a hash/object)
+
+types of responses that we can get from servers:
+* html
+* images
+* javascript
+* css
+
+* json
+lets us send data between servers and clients
+
+## Gems
+
+external libraries (code we didn't write)
+we can use to do something
+
+```
+gem install some-name
+```
+
+downloading the code onto our computer - `require` finds that code, loads it
+
+parse - take apart a string, turn it into something more useful
+
+
+## API
+
+Application Programming Interface
+
+HTTP RESTful JSON API - some web server that gives us data (lets us create data) on some service
+
+Google Books API
+
+
+## HTTP URLs
+
+`http://...`
+`?q=quilting&limit=5`
+q: quilting
+limit: 5
 
 ### Deliverables
 
 * Write an application that takes a search term from a user
+
+use gets
+
 * Make a Request to the GoogleBooks API and get back some results
   * https://www.googleapis.com/books/v1/volumes?q=ruby+programming
+  
 * Display the titles, author names, and description for each book
--- 02-hashes-and-the-internet/app.rb
+++ 02-hashes-and-the-internet/app.rb
@@ -1,2 +1,59 @@
-# require 'rest-client'
-# require 'json'
+# ### Deliverables
+#
+# * Write an application that takes a search term from a user
+#
+# use gets
+#
+# * Make a Request to the GoogleBooks API and get back some results
+#   * https://www.googleapis.com/books/v1/volumes?q=ruby+programming
+#
+# * Display the titles, author names, and description for each book
+
+require 'rest-client'
+require 'json'
+require 'pry'
+
+# makes URL a constant, defined everywhere
+URL = "https://www.googleapis.com/books/v1/volumes?q="
+
+# STEPS:
+# get the user input
+
+def get_user_input
+  puts "What kinds of books are you interested in?"
+  gets.chomp
+end
+
+# get the data that we want
+def get_data_from_books_api
+  input = get_user_input
+  url_to_fetch = URL + input
+  RestClient.get(url_to_fetch).body
+end
+
+# parse the data - format correctly
+def parse_data
+  JSON.parse(get_data_from_books_api)
+end
+
+# dig into the data to find specific pieces we want
+# title, author name, description for each book
+def print_one_book(book)
+  volume_info = book["volumeInfo"]
+  puts volume_info["title"]
+  puts volume_info["authors"]
+  puts volume_info["description"]
+end
+
+# loop through all of the books
+def get_and_print_books
+  result = parse_data
+  result["items"].each do |book|
+    print_one_book(book)
+    puts "\n"
+  end
+end
+
+# binding.pry
+puts "hi"
+get_and_print_books
