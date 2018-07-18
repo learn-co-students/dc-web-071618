a2a3cea8fb17e7040c890441420a44477b2e35e6
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-07-18 10:36:48 -0400

    post lecture 03-intro-to-oo

---
+++ 03-intro-to-oo/sandbox.rb
@@ -0,0 +1,20 @@
+# x = 3
+
+# message_name = :+
+
+
+# # puts x.class
+
+# # puts (x + 2)
+# # puts x.send(message_name, 2)
+
+# test_array = ["Bob", "Mary", "Hepzibah"]
+
+# puts test_array[0]
+
+# puts test_array.[](0)
+# puts test_array.send(:[], 0)
+
+ella = {name: "Ella", color: "black", floofiness: 10}
+
+puts ella[:name]
