075fb0a4241b3e0eb7b9bbf51ad3e2a2eb0693ac
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-08-06 15:49:45 -0400

    post lecture

--- 13-intro-to-sinatra/app/controllers/application_controller.rb
+++ 13-intro-to-sinatra/app/controllers/application_controller.rb
@@ -1,3 +1,35 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
+
+    set :views, "app/views"
+
+    get "/" do
+        erb :home
+    end
+
+    get "/books" do
+        @books = Book.all
+        erb :index
+    end
+
+    get "/books/new" do
+        erb :new
+    end
+
+    post "/books" do
+        author = params[:author]
+        title = params[:title]
+        snippet = params[:snippet]
+        book = Book.create(author: author, title: title, snippet: snippet)
+        redirect "/books/#{book.id}"
+    end
+
+    get "/books/:id" do
+        book_id = params[:id]
+        @book = Book.find(book_id)
+        erb :show
+    end
+
 
 end
