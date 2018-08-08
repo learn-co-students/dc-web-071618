9ec7942c20a26a093bf9e94c3b3c258407b977f2
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-08-08 17:32:51 -0400

    post lecture

--- 15-sinatra-associated-objects/app/controllers/authors_controller.rb
+++ 15-sinatra-associated-objects/app/controllers/authors_controller.rb
@@ -3,4 +3,32 @@ class AuthorsController < ApplicationController
     set :views, "app/views/authors"
 
 
+    get "/authors" do 
+        @authors = Author.all 
+        erb :index
+    end
+
+    get "/authors/new" do
+        # load a blank author form
+        erb :new
+    end
+
+    post "/authors" do
+        # create a new author
+        author_name = params[:author_name]
+        author = Author.find_or_create_by(name: author_name)
+        params[:book].each do |book_info|
+            book_title = book_info[:title]
+            book_snippet = book_info[:snippet]
+            Book.create(author: author, title: book_title, snippet: book_snippet)
+        end
+        redirect "/authors/#{author.id}"
+    end
+
+    get "/authors/:id" do
+        @author = Author.find(params[:id])
+        erb :show
+    end
+
+
 end
