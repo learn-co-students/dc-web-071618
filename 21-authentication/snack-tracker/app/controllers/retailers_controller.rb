0f60567b7e67f34a12f67facde0ea22d05ef6c3d
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-08-21 13:30:13 -0400

    prelecture 21

---
+++ 21-authentication/snack-tracker/app/controllers/retailers_controller.rb
@@ -0,0 +1,37 @@
+class RetailersController < ApplicationController
+
+  def index
+    @retailers = Retailer.all
+  end
+
+  def show
+    @retailer = Retailer.find(params[:id])
+  end
+
+  def new
+    @retailer = Retailer.new
+    @retailer.snacks.build 
+  end
+
+  def create
+    @retailer = Retailer.create(accepted_retailer_form_fields)
+    if @retailer.errors
+      render :new
+    else
+      redirect_to retailer_path(@retailer)
+    end
+  end
+
+  def destroy
+    @retailer = Retailer.find(params[:id])
+    @retailer.destroy
+    redirect_to retailers_path
+  end
+
+  private
+
+  def accepted_retailer_form_fields
+    params.require(:retailer).permit(:name, :year_established,
+        snacks_attributes: [:name, :calories, :deliciousness])
+  end
+end
