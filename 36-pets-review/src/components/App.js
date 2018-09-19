b715166dffee1dab1c3e22024cdca97ccf45da78
github username <sam.catherman@gmail.com>
2018-09-19 17:42:19 -0400

    pets review

---
+++ 36-pets-review/src/components/App.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+import Filters from "./Filters";
+import PetBrowser from "./PetBrowser";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      pets: [],
+      filters: {
+        type: "all"
+      }
+    };
+  }
+
+  onChangeType = type => {
+    this.setState({ filters: { ...this.state.filters, type: type } });
+  };
+
+  fetchAllPets = () => {
+    fetch(`/api/pets`)
+      .then(res => res.json())
+      .then(pets => this.setState({ pets }));
+  };
+
+  fetchPetsByType = type => {
+    if (this.state.filters.type === "all") {
+      this.fetchAllPets();
+    } else {
+      fetch(`/api/pets?type=${type}`)
+        .then(res => res.json())
+        .then(pets => this.setState({ pets }));
+    }
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <header>
+          <h1 className="ui dividing header">React Animal Shelter</h1>
+        </header>
+        <div className="ui container">
+          <div className="ui grid">
+            <div className="four wide column">
+              <Filters
+                onChangeType={this.onChangeType}
+                fetchPets={() => this.fetchPetsByType(this.state.filters.type)}
+              />
+            </div>
+            <div className="twelve wide column">
+              <PetBrowser petProps={this.state.pets} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ 36-pets-review/src/components/Pet.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+class Pet extends React.Component {
+  render() {
+    console.log(this.props);
+    const pet = this.props.petProp;
+    return (
+      <div className="card">
+        <div className="content">
+          <a className="header">
+            {/*'' OR '' */}
+            {pet.gender === "female" ? "♀" : "♂"}
+            {pet.name}
+          </a>
+          <div className="meta">
+            <span className="date">{pet.type}</span>
+          </div>
+          <div className="description">
+            <p>Age: {pet.age}</p>
+            <p>Weight: {pet.weight}</p>
+          </div>
+        </div>
+        <div className="extra content">
+          <button className="ui disabled button">Already adopted</button>
+          <button className="ui primary button">Adopt pet</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Pet;
